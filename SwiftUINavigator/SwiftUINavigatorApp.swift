4566da53375420e682a2fab71a393e0bfe6a73d1
Lee Burrows <lee.burrows@e.lloydsbanking.com>
2023-08-16 09:13:16 +0100

    Initial Commit

---
+++ SwiftUINavigator/SwiftUINavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigatorApp.swift
+//  SwiftUINavigator
+//
+//  Created by Burrows, Lee (Everyday B INV - Internals Lloyds, Consumer Relationships Platform) on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
